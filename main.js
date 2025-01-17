0dca3afebd1e28f52beacb63667a4fa269d4fa4a
Stepan Trubcov <stepantrubcov@gmail.com>
2025-01-17 16:36:37 +0300

    добавление кнопки пройти текст

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 import { Telegraf, Markup } from "telegraf";
 
+const STICKER_ID = 'CAACAgIAAxkBAAELshlnijBYb5muDGZjYrpaBeDgiCulVQACfwoBAAHsKUhIgGGW2YO3xco2BA';
 const token = "7919809731:AAHR9IPjbFtZondrgGKtZrP5d6L_b_vsTvA";
 
 const bot = new Telegraf(token);
 
-bot.start((ctx) => {
-  ctx.reply("Привет, давай изучать крюки! Вызовите эту команду /Izuchat_Kruki");
+bot.command('Izuchat Kruki', (ctx) => {
+    ctx.replyWithSticker(STICKER_ID);
+    ctx.reply("Что это за крюк");
 });
 
-bot.command("Izuchat_Kruki", (ctx) => {
-  ctx.reply("Что это за крюк");
-});
+bot.start((ctx) => {
+    return ctx.reply(
+        'Привет, давай изучать крюки!',
+        Markup.inlineKeyboard([
+            Markup.button.callback("Пройти тест", "like"),
+        ])
+    )
+})
 
 bot.on("message", (ctx) => {
-  ctx.reply("Неправильный ответ");
+    ctx.reply("Неправильный ответ");
 
-  console.log("Сообщение юзера:", ctx.message.sticker);
+    console.log("Сообщение юзера:", ctx.message.sticker);
 });
 
 bot.launch();
