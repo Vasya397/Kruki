863a43725a95b3096173f98e4b01b04e24bd6c24
Василий Гладышев <v.gladyshev@roottech.ru>
2025-01-20 12:51:57 +0300

    Добавил базу, связал её, теперь есть правильные ответы и вопросы

--- main.js
+++ main.js
@@ -1,32 +1,60 @@
 import { Telegraf, Markup } from "telegraf";
+import baza from "./baza.json" assert { type: "json" };
 
-
-const STICKER_ID = 'CAACAgIAAxkBAAELshlnijBYb5muDGZjYrpaBeDgiCulVQACfwoBAAHsKUhIgGGW2YO3xco2BA';
 const token = "7919809731:AAHR9IPjbFtZondrgGKtZrP5d6L_b_vsTvA";
 
 const bot = new Telegraf(token);
 
-const keyboard = [
-    [{ text: 'Пройти тест', callback_data: 'button_click' }]
-];
-
-
-bot.on('callback_query', (ctx) => {
-    ctx.answerCbQuery();
-    ctx.replyWithSticker(STICKER_ID);
-    ctx.reply("Что это за крюк");
+bot.start((ctx) => {
+  return ctx.reply("Привет, давай изучать крюки!", {
+    reply_markup: {
+      inline_keyboard: keyboard,
+    },
+  });
 });
 
-bot.start((ctx) => {
-    return ctx.reply(
-        'Привет, давай изучать крюки!',
-        {
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        },
+const keyboard = [[{ text: "Пройти тест", callback_data: "button_click" }]];
 
-    )
-})
+const userProgress = {};
+
+bot.on("callback_query", (ctx) => {
+  const userId = ctx.from.id;
+  userProgress[userId] = 0;
+
+  sendQuestion(ctx, userId);
+});
+
+bot.on("text", (ctx) => {
+  const userId = ctx.from.id;
+
+  const userAnswer = ctx.message.text;
+  const currentQuestionIndex = userProgress[userId];
+  const currentQuestion = baza[currentQuestionIndex];
+
+  if (currentQuestion.answer.includes(userAnswer)) {
+    userProgress[userId]++;
+    ctx.reply("Правильно");
+  } else {
+    userProgress[userId]++;
+    ctx.reply("Чел, иди в духовку учись");
+  }
+
+  if (userProgress[userId] !== undefined) {
+    userProgress[userId]++;
+
+    if (userProgress[userId] < baza.length) {
+      sendQuestion(ctx, userId);
+    } else {
+      ctx.reply("Тест завершен!");
+      delete userProgress[userId];
+    }
+  }
+});
+
+function sendQuestion(ctx, userId) {
+  const question = baza[userProgress[userId]];
+  ctx.reply(question.text);
+  ctx.replyWithSticker(question.sticker);
+}
 
 bot.launch();
