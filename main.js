3c7a6e8314eae067547887d3f38d4de97e2a20dc
Василий Гладышев <v.gladyshev@roottech.ru>
2025-01-21 23:01:48 +0300

    Добавил на результат теста разные стикеры

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { Telegraf, Markup } from "telegraf";
 import baza from "./baza.json" assert { type: "json" };
 import shuffle from "./shuffle.js";
+import answerStik from "./answerStik.json" assert { type: "json" };
 
 const token = "7919809731:AAHR9IPjbFtZondrgGKtZrP5d6L_b_vsTvA";
 
@@ -16,7 +17,9 @@ bot.start((ctx) => {
 
 const keyboard = [[{ text: "Пройти тест", callback_data: "button_click" }]];
 
-const userProgress = {}
+const userProgress = {};
+
+let numTrueAnswer = 0;
 
 bot.on("callback_query", (ctx) => {
   const userId = ctx.from.id;
@@ -26,24 +29,38 @@ bot.on("callback_query", (ctx) => {
     userProgress[userId] = 0;
   }
 
-  if (buttonData === 'true_answer') {
+  if (buttonData === "true_answer") {
+    userProgress[userId]++;
+    numTrueAnswer++;
+  }
+  if (buttonData === "false_2") {
     userProgress[userId]++;
   }
-  if (buttonData === 'false_2') {
+  if (buttonData === "false_3") {
     userProgress[userId]++;
   }
-  if (buttonData === 'false_3') {
+  if (buttonData === "false_4") {
     userProgress[userId]++;
   }
-  if (buttonData === 'false_4') {
-    userProgress[userId]++;
-  }
-
 
   if (userProgress[userId] < baza.length) {
     sendQuestion(ctx, userId);
   } else {
     ctx.reply("Тест завершен!");
+
+    if (numTrueAnswer >= 0 && numTrueAnswer <= 4) {
+      ctx.reply(`Ты ответил на ${numTrueAnswer} из ${baza.length}`);
+      ctx.replyWithSticker(answerStik.id_0_4);
+    } else if (numTrueAnswer >= 5 && numTrueAnswer <= 7) {
+      ctx.reply(`Ты ответил на ${numTrueAnswer} из ${baza.length}`);
+      ctx.replyWithSticker(answerStik.id_5_7);
+    } else if (numTrueAnswer >= 8 && numTrueAnswer <= 9) {
+      ctx.reply(`Ты ответил на ${numTrueAnswer} из ${baza.length}`);
+      ctx.replyWithSticker(answerStik.id_8_9);
+    } else if (numTrueAnswer == 10) {
+      ctx.reply(`Ты ответил на ${numTrueAnswer} из ${baza.length}`);
+      ctx.replyWithSticker(answerStik.id_10);
+    }
     delete userProgress[userId];
   }
 });
@@ -51,22 +68,18 @@ bot.on("callback_query", (ctx) => {
 function sendQuestion(ctx, userId) {
   const question = baza[userProgress[userId]];
   const answer = [
-    { text: question.answer[0], callback_data: 'true_answer' },
-    { text: question.answer[1], callback_data: 'false_2' },
-    { text: question.answer[2], callback_data: 'false_3' },
-    { text: question.answer[3], callback_data: 'false_4' }
-  ]
-  const mixer = shuffle(answer)
+    { text: question.answer[0], callback_data: "true_answer" },
+    { text: question.answer[1], callback_data: "false_2" },
+    { text: question.answer[2], callback_data: "false_3" },
+    { text: question.answer[3], callback_data: "false_4" },
+  ];
+  const mixer = shuffle(answer);
   ctx.replyWithSticker(question.sticker);
-  ctx.reply(question.text,
-    {
-      reply_markup: {
-        inline_keyboard: [
-          mixer
-        ]
-      }
-    }
-  )
+  ctx.reply(question.text, {
+    reply_markup: {
+      inline_keyboard: [mixer],
+    },
+  });
 }
 
 bot.launch();
