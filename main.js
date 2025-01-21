5d5b47d7c947761bb83814ef093988a38d7d983a
Stepan Trubcov <stepantrubcov@gmail.com>
2025-01-21 17:30:09 +0300

    Добавил четыре врянта ответа по кнопке

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { Telegraf, Markup } from "telegraf";
 import baza from "./baza.json" assert { type: "json" };
+import shuffle from "./shuffle.js";
 
 const token = "7919809731:AAHR9IPjbFtZondrgGKtZrP5d6L_b_vsTvA";
 
@@ -15,46 +16,57 @@ bot.start((ctx) => {
 
 const keyboard = [[{ text: "Пройти тест", callback_data: "button_click" }]];
 
-const userProgress = {};
+const userProgress = {}
 
 bot.on("callback_query", (ctx) => {
   const userId = ctx.from.id;
-  userProgress[userId] = 0;
+  const buttonData = ctx.callbackQuery.data;
 
-  sendQuestion(ctx, userId);
-});
-
-bot.on("text", (ctx) => {
-  const userId = ctx.from.id;
-
-  const userAnswer = ctx.message.text;
-  const currentQuestionIndex = userProgress[userId];
-  const currentQuestion = baza[currentQuestionIndex];
-
-  if (currentQuestion.answer.includes(userAnswer)) {
-    userProgress[userId]+ 1;
-    ctx.reply("Правильно");
-  } else {
-    userProgress[userId]+ 1;
-    ctx.reply("Чел, иди в духовку учись");
+  if (userProgress[userId] === undefined) {
+    userProgress[userId] = 0;
   }
 
-  if (userProgress[userId] !== undefined) {
+  if (buttonData === 'true_answer') {
     userProgress[userId]++;
+  }
+  if (buttonData === 'false_2') {
+    userProgress[userId]++;
+  }
+  if (buttonData === 'false_3') {
+    userProgress[userId]++;
+  }
+  if (buttonData === 'false_4') {
+    userProgress[userId]++;
+  }
 
-    if (userProgress[userId] < baza.length) {
-      sendQuestion(ctx, userId);
-    } else {
-      ctx.reply("Тест завершен!");
-      delete userProgress[userId];
-    }
+
+  if (userProgress[userId] < baza.length) {
+    sendQuestion(ctx, userId);
+  } else {
+    ctx.reply("Тест завершен!");
+    delete userProgress[userId];
   }
 });
 
 function sendQuestion(ctx, userId) {
   const question = baza[userProgress[userId]];
-  ctx.reply(question.text);
+  const answer = [
+    { text: question.answer[0], callback_data: 'true_answer' },
+    { text: question.answer[1], callback_data: 'false_2' },
+    { text: question.answer[2], callback_data: 'false_3' },
+    { text: question.answer[3], callback_data: 'false_4' }
+  ]
+  const mixer = shuffle(answer)
   ctx.replyWithSticker(question.sticker);
+  ctx.reply(question.text,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          mixer
+        ]
+      }
+    }
+  )
 }
 
 bot.launch();
---
+++ shuffle.js
@@ -0,0 +1,24 @@
+const shuffle = (array) => {
+  if(array.length > 5) return array.length
+    let m = array.length, t, i;
+    while (m) {
+  
+      i = Math.floor(Math.random() * m--);
+  
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+  }
+
+//   function shuffleArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     return array;
+// }
+
+  export default shuffle;
